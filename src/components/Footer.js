66f081fa42b6ba03db1377c3e70af4043c3f42a9
AnnaBilokon <Agnewka89@gmail.com>
2023-09-20 09:08:49 +0200

    creating the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/assets/images/Logo-12.png';
 
-const Footer = () => {
-  return (
-	<div>
-	  Footer
-	</div>
-  )
-}
-
-export default Footer
+const Footer = () => (
+	<Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '150px', height: '100px' }} />
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="30px" textAlign="center" pb="40px">Made with ❤️ by Anna Bilokon</Typography>
+  </Box>
+)
+export default Footer;
